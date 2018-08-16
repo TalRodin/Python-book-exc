c1a852d9c06f1cd9fab8a0f0402dd7be21aeaae6
alyonarodin <alyona.rodin@gmail.com>
2018-08-16 16:08:08 -0400

    added

--- algorithm_9.py
+++ algorithm_9.py
@@ -34,42 +34,74 @@ set([10,20,30,40])
 
 #6
 
-set1=([1,2,3])
-set2=([10,20,30])
-set3=set1+set2
+set1=set([1,2,3])
+set2=set([10,20,30])
+set3=set1.union(set2)
 print(set3)
 
 #7
 
-set1=([1,2,3,10])
-set2=([10,20,30])
+set1=set([1,2,3,10])
+set2=set([10,20,30])
 set3=set1&set2
 print(set3)
 
 #8
 
-set1=([1,2,3,10])
-set2=([10,20,30])
+set1=set([1,2,3,10])
+set2=set([10,20,30])
 set3=set1-set2
 print(set3)
 
 #9
 
-set1=([1,2,3,10])
-set2=([10,20,30])
+set1=set([1,2,3,10])
+set2=set([10,20,30])
 set3=set2-set1
 print(set3)
 
 #10
 
-set1=([1,2,3,10])
-set2=([10,20,30])
+set1=set([1,2,3,10])
+set2=set([10,20,30])
 set3=set2^set1
 print(set3)
 
 #11
 
-
+import pickle
+d={}
+d['a']=1
+d['b']=2
+d['c']=3
+output_file=open('mydata.dat','wb')
+pickle.dump(d,output_file)
+output_file.close()
 
 #12
+
+input_file=open('mydata.dat','rb')
+pb=pickle.load(input_file)
+print(pb)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
